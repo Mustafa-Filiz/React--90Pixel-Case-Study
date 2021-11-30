00b5bdbac70f431de820da7aafb0d43d30364d93
Mustafa Filiz <mustafafiliz18@gmail.com>
2021-11-30 23:53:42 +0300

    updates

--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -2,6 +2,9 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from '../pages/Login';
 import Signup from '../pages/Signup';
+import UserDetail from '../pages/UserDetail';
+import Users from '../pages/Users';
+import PrivateRouter from './PrivateRouter';
 
 function AppRouter() {
     return (
@@ -9,6 +12,15 @@ function AppRouter() {
             <Routes>
                 <Route path="/" element={<Login />} />
                 <Route path="signup" element={<Signup />} />
+                <Route
+                    path="users"
+                    element={
+                        <PrivateRouter>
+                            <Users />
+                        </PrivateRouter>
+                    }
+                />
+                <Route path="users/:id" element={<UserDetail />} />
             </Routes>
         </Router>
     );
