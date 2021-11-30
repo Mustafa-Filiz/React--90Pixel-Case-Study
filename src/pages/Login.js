e856eb00d8e4bd3cc0223634c0812c79d3946308
Mustafa Filiz <mustafafiliz18@gmail.com>
2021-11-30 23:53:31 +0300

    updates

---
+++ src/pages/Login.js
@@ -0,0 +1,88 @@
+import React, { useEffect } from 'react';
+import { Form, Input, Button } from 'antd';
+import { LockOutlined, MailOutlined } from '@ant-design/icons';
+import { Link, useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchUsersAsync, userLogin } from '../redux/userSlice';
+
+function Login() {
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+    const users = useSelector((state) => state.users.users);
+
+    useEffect(() => {
+        dispatch(fetchUsersAsync());
+    }, [dispatch]);
+
+    const onFinish = (values) => {
+        dispatch(userLogin(values));
+        navigate('users');
+    };
+
+    return (
+        <div>
+            <h1>Welcome to my app.</h1>
+            <h3>Please login or sign up to see user info.</h3>
+            <Form
+                name="normal_login"
+                className="login-form"
+                initialValues={{
+                    remember: true,
+                }}
+                onFinish={onFinish}
+                size="large"
+            >
+                <Form.Item
+                    name="email"
+                    rules={[
+                        {
+                            required: true,
+                            message: 'Please input your Email!',
+                        },
+                        {
+                            type: 'email',
+                            message: 'The input is not valid E-mail!',
+                        },
+                    ]}
+                >
+                    <Input
+                        prefix={
+                            <MailOutlined className="site-form-item-icon" />
+                        }
+                        placeholder="Email"
+                    />
+                </Form.Item>
+                <Form.Item
+                    name="password"
+                    rules={[
+                        {
+                            required: true,
+                            message: 'Please input your Password!',
+                        },
+                    ]}
+                >
+                    <Input
+                        prefix={
+                            <LockOutlined className="site-form-item-icon" />
+                        }
+                        type="password"
+                        placeholder="Password"
+                    />
+                </Form.Item>
+
+                <Form.Item>
+                    <Button
+                        type="primary"
+                        htmlType="submit"
+                        className="login-form-button"
+                    >
+                        Log in
+                    </Button>{' '}
+                    or <Link to="signup">Register Now!</Link>
+                </Form.Item>
+            </Form>
+        </div>
+    );
+}
+
+export default Login;
