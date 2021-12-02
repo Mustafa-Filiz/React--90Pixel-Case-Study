f9c4241edd66f54665ad3ab5154b489f71024e78
Mustafa Filiz <mustafafiliz18@gmail.com>
2021-12-02 23:21:49 +0300

    updates

--- src/pages/UserDetail.js
+++ src/pages/UserDetail.js
@@ -2,7 +2,9 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import { useParams } from 'react-router';
 import { usersSelector } from '../redux/userSlice';
-import { Descriptions, Badge } from 'antd';
+import { Card } from 'antd';
+
+const { Meta } = Card;
 
 function UserDetail() {
     const { id } = useParams();
@@ -12,47 +14,22 @@ function UserDetail() {
     console.log(detailedUser);
 
     return (
-        <Descriptions title="User Info" bordered>
-            <Descriptions.Item>
-                <img src={detailedUser.image} alt="profile-pic" />
-            </Descriptions.Item>
-            <Descriptions.Item label="Name">
-                {detailedUser.name}
-            </Descriptions.Item>
-            <Descriptions.Item label="Email">
-                {detailedUser.email}
-            </Descriptions.Item>
-            <Descriptions.Item label="Automatic Renewal">YES</Descriptions.Item>
-            <Descriptions.Item label="Order time">
-                2018-04-24 18:00:00
-            </Descriptions.Item>
-            <Descriptions.Item label="Usage Time" span={2}>
-                2019-04-24 18:00:00
-            </Descriptions.Item>
-            <Descriptions.Item label="Status" span={3}>
-                <Badge status="processing" text="Running" />
-            </Descriptions.Item>
-            <Descriptions.Item label="Negotiated Amount">
-                $80.00
-            </Descriptions.Item>
-            <Descriptions.Item label="Discount">$20.00</Descriptions.Item>
-            <Descriptions.Item label="Official Receipts">
-                $60.00
-            </Descriptions.Item>
-            <Descriptions.Item label="Config Info">
-                Data disk type: MongoDB
-                <br />
-                Database version: 3.4
-                <br />
-                Package: dds.mongo.mid
-                <br />
-                Storage space: 10 GB
-                <br />
-                Replication factor: 3
-                <br />
-                Region: East China 1<br />
-            </Descriptions.Item>
-        </Descriptions>
+        <Card
+            style={{ width: 500, margin: 'auto' }}
+            cover={<img alt="profile-pic" src={detailedUser.image} />}
+        >
+            <Meta
+                title={detailedUser.name}
+                description={
+                    <div>
+                        <p>{detailedUser.email.toLowerCase()}</p>
+                        <p>Country: {detailedUser.country}</p>
+                        <p>Company: {detailedUser.company}</p>
+                        <p>Job: {detailedUser.job}</p>
+                    </div>
+                }
+            />
+        </Card>
     );
 }
 
